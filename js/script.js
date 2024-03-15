39df6be36052e2d1e14f868053d4e398101f7476
simone-danzeri <s.danzeri@icloud.com>
2024-03-15 14:25:10 +0100

    feat:  select DOM elements and ready to click for play button

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// Mi seleziono i due elementi del DOM che mi servono e li metto in una variabile
+const playBtn = document.querySelector('#play-btn');
+console.log(playBtn);
+const grid = document.querySelector('#grid');
+console.log(grid);
+// Metto in ascolto il bottone del play
+playBtn.addEventListener('click', function() {
+    
+});
+
+
+
+// FUNCTIONS
